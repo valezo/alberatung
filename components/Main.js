aa7b85adc9e203ba6103ce0622f0b22665786897
Arbnore1 <bekaarbnore@gmail.com>
2024-12-10 23:27:00 +0100

    edited page description

--- components/Main.js
+++ components/Main.js
@@ -4,7 +4,7 @@ export default function Main() {
       <div className="max-w-7xl mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 md:ml-24 pt-6 flex flex-col md:items-start md:text-left mb-40 items-center text-center">
           <h1 className="mb-5 sm:text-6xl text-5xl items-center Avenir xl:w-2/2 text-gray-900">
-            We are making Stunning Websites
+            We are doing an amazing work
           </h1>
           <p className="mb-4 xl:w-3/4 text-gray-600 text-lg">
             nine4 is a free to use website template for websites made with
