a6ccd00d1514247d2229f397ba38d77ef77af296
Arbnore1 <bekaarbnore@gmail.com>
2024-12-14 22:05:52 +0100

    updated header and subheader, removed  unnessesary items, and add the map

--- components/Main.js
+++ components/Main.js
@@ -1,115 +1,126 @@
 export default function Main() {
   return (
-    <section className="text-gray-600 body-font">
-      <div className="max-w-7xl mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 md:ml-24 pt-6 flex flex-col md:items-start md:text-left mb-40 items-center text-center">
-          <h1 className="sm:text-5xl text-5xl items-center Avenir xl:w-2/2 text-gray-900">
-            Kush eshte
-          </h1>
-          <div className="mt-0 mb-0">
-            <img src="/images/logo.jpg" width={"240px"} alt="" />
-          </div>
-          <p className="mb-4 xl:w-3/4 text-gray-600 text-lg">
-            AL BERATUNG- është një kompani e cila ka për qëllim t'u ndihmojë në
-            lidhje me përgaditjen e dokumenteve që duhet dorëzuar në
-            institucione shtetërore.
-          </p>
-          <div className="flex justify-center">
-            <a
-              className="inline-flex items-center px-5 py-3 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-gray-900"
-              href="https://github.com/r1/nine4-2/"
-            >
-              <span className="justify-center">Zbuloni më shumë →</span>
-            </a>
-          </div>
-        </div>
-        <div className="xl:mr-44 sm:mr-0 sm:mb-28 mb-0 lg:mb-0 mr-48 md:pl-10">
-          <img
-            className="w-80 md:ml-1 ml-24"
-            alt="iPhone-12"
-            src="/images/iPhone-12-Mockup.png"
-          ></img>
-        </div>
-      </div>
-      <section className="mx-auto">
-        <div className="container px-5 mx-auto lg:px-24 ">
-          <div className="flex flex-col w-full mb-4 text-left lg:text-center">
-            <h1 className="mb-8 text-2xl Avenir font-semibold text-black">
-              Trusted by top-tier product companies
-            </h1>
-          </div>
-          <div className="grid grid-cols-2 gap-16 mb-16 text-center lg:grid-cols-4">
-            <div className="flex items-center justify-center">
-              <img
-                src="/images/Google-Logo.webp"
-                alt="Google Logo"
-                className="block object-contain h-16 greyC"
-              ></img>
-            </div>
-            <div className="flex items-center justify-center">
-              <img
-                src="/images/Shopify-Logo.svg"
-                alt="Shopify Logo"
-                className="block object-contain h-16 greyC"
-              ></img>
-            </div>
-            <div className="flex items-center justify-center">
-              <img
-                src="/images/Cloudflare-Logo.svg"
-                alt="Cloudflare Logo"
-                className="block object-contain h-16 greyC"
-              ></img>
-            </div>
-            <div className="flex items-center justify-center">
-              <img
-                src="/images/PayPal-Logo.png"
-                alt="Paypal Logo"
-                className="block object-contain h-16 greyC"
-              ></img>
-            </div>
-          </div>
-        </div>
-      </section>
-      <div className="grr max-w-7xl pt-20 mx-auto text-center">
-        <h1 className="mb-8 text-6xl Avenir font-semibold text-gray-900">
-          Less code, less effort.
-        </h1>
-        <h1 className="mb-8 text-2xl Avenir font-semibold text-gray-600 text-center">
-          Minify your CSS with Tailwind's built in PostCSS support.
-        </h1>
-        <div className="container flex flex-col items-center justify-center mx-auto rounded-lg ">
-          <img
-            className="object-cover object-center w-3/4 mb-10 g327 border rounded-lg shadow-md"
-            alt="Placeholder Image"
-            src="./images/placeholder.png"
-          ></img>
-        </div>
-      </div>
-      <section className="relative">
-        <div className="max-w-6xl mx-auto px-4 sm:px-6 text-center">
-          <div className="py-24 md:py-36">
-            <h1 className="mb-5 text-6xl Avenir font-semibold text-gray-900">
-              Subscribe to our newsletter
-            </h1>
-            <h1 className="mb-9 text-2xl font-semibold text-gray-600">
-              Enter your email address and get our newsletters straight away.
-            </h1>
-            <input
-              placeholder="jack@example.com"
-              name="email"
-              type="email"
-              autoComplete="email"
-              className="border border-gray-600 w-1/4 pr-2 pl-2 py-3 mt-2 rounded-md text-gray-800 font-semibold hover:border-gray-900"
-            ></input>{" "}
-            <a
-              className="inline-flex items-center px-14 py-3 mt-2 ml-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-gray-900"
-              href="/"
-            >
-              <span className="justify-center">Subscribe</span>
-            </a>
-          </div>
-        </div>
-      </section>
-    </section>
+  <section className="text-gray-600 body-font">
+  <div className="max-w-7xl mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+  <div className="lg:flex-grow md:w-1/2 md:ml-24 pt-6 flex flex-col md:items-start md:text-left mb-40 items-center text-center">
+  <h1 className="sm:text-7xl text-7xl items-center Avenir xl:w-2/2 text-gray-900 mx-auto">
+  Kush eshte
+  </h1>
+  <div className="mt-0 mb-0 mx-auto" >
+  <img src="/images/logo.jpg" width={"240px"}/>
+  </div>
+  <p className="mb-4 xl:w-2/3 text-gray-600 text-lg text-center mx-auto">
+  Kompani e cila ka për qëllim t'u ndihmojë në
+  lidhje me përgaditjen e dokumenteve që duhet dorëzuar në
+  institucione shtetërore.
+  </p>
+  <h3 className="mb-3 xl:w-2/3  text-gray-600 text-lg text-center mx-auto">
+Ne ju ofrojmë mbështetje profesionale në kohë optimale pa pasur nevojë për pritje të gjata për termine. Shumica e shërbimeve tona ofrohen online, natyrisht ne ju mirëpresim edhe në zyrën tonë.  
+</h3> 
+
+<p className="mb-5 xl:w-4/5 text-black text-5xl text-center mx-auto mt-11">
+<b> Konsultohu shqip</b>
+</p> 
+<p className="mb-4 xl:w-2/3  text-gray-600 text-lg text-center mx-auto"> Me një gamë të gjerë, me përkushtim dhe profesionalizëm, ju kryeni punët tuaja shpejt, dhe më e mira, ju merreni vesh me konsultentin tuaj shqip. </p>
+{/* Me një gamë të gjerë shërbimesh, me përkushtim dhe profesionalizëm, ju kryeni punët tuaja shpejt, dhe më e mira, ju merreni vesh me konsulentin tuaj shqip. */}
+  {/* <div className="flex justify-center">
+  <a
+  className="inline-flex items-center px-5 py-3 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-gray-900"
+  href="https://github.com/r1/nine4-2/"
+  >
+  <span className="justify-center">Zbuloni më shumë →</span>
+  </a>
+  </div> */}
+  </div>
+  {/* <div className="xl:mr-44 sm:mr-0 sm:mb-28 mb-0 lg:mb-0 mr-48 md:pl-10">
+  <img
+  className="w-80 md:ml-1 ml-24"
+  alt="iPhone-12"
+  src="/images/iPhone-12-Mockup.png"
+  ></img>
+  </div> */}
+  </div>
+  {/* <section className="mx-auto">
+  <div className="container px-5 mx-auto lg:px-24 ">
+  <div className="flex flex-col w-full mb-4 text-left lg:text-center">
+  <h1 className="mb-8 text-2xl Avenir font-semibold text-black">
+  Trusted by top-tier product companies
+  </h1>
+  </div>
+  <div className="grid grid-cols-2 gap-16 mb-16 text-center lg:grid-cols-4">
+  <div className="flex items-center justify-center">
+  <img
+  src="/images/Google-Logo.webp"
+  alt="Google Logo"
+  className="block object-contain h-16 greyC"
+  ></img>
+  </div>
+  <div className="flex items-center justify-center">
+  <img
+  src="/images/Shopify-Logo.svg"
+  alt="Shopify Logo"
+  className="block object-contain h-16 greyC"
+  ></img>
+  </div>
+  <div className="flex items-center justify-center">
+  <img
+  src="/images/Cloudflare-Logo.svg"
+  alt="Cloudflare Logo"
+  className="block object-contain h-16 greyC"
+  ></img>
+  </div>
+  <div className="flex items-center justify-center">
+  <img
+  src="/images/PayPal-Logo.png"
+  alt="Paypal Logo"
+  className="block object-contain h-16 greyC"
+  ></img>
+  </div>
+  </div>
+  </div>
+  </section> */}
+  <div className="grr max-w-7xl pt-20 mx-auto text-center">
+  <h1 className="mb-8 text-6xl Avenir font-semibold text-gray-900">
+  Ku gjendemi ne?
+  </h1>
+  <h1 className="mb-8 text-2xl Avenir font-semibold text-gray-600 text-center">
+  Filderstadt, Germany
+  </h1>
+  <div className="container flex flex-col items-center justify-center mx-auto rounded-lg">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2635.804866308403!2d9.195735376452548!3d48.65184807130486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDjCsDM5JzA2LjciTiA5wrAxMSc1My45IkU!5e0!3m2!1sen!2s!4v1734206819635!5m2!1sen!2s" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  {/* <img
+  className="object-cover object-center w-3/4 mb-10 g327 border rounded-lg shadow-md"
+  alt="Placeholder Image"
+  src="./images/placeholder.png"
+  ></img> */}
+  </div>
+  </div>
+  <section className="relative">
+  <div className="max-w-6xl mx-auto px-4 sm:px-6 text-center">
+  <div className="py-24 md:py-36">
+  <h1 className="mb-5 text-6xl Avenir font-semibold text-gray-900">
+  Subscribe to our newsletter
+  </h1>
+  <h1 className="mb-9 text-2xl font-semibold text-gray-600">
+  Enter your email address and get our newsletters straight away.
+  </h1>
+  <input
+  placeholder="jack@example.com"
+  name="email"
+  type="email"
+  autoComplete="email"
+  className="border border-gray-600 w-1/4 pr-2 pl-2 py-3 mt-2 rounded-md text-gray-800 font-semibold hover:border-gray-900"
+  ></input>{" "}
+  <a
+  className="inline-flex items-center px-14 py-3 mt-2 ml-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-gray-900"
+  href="/"
+  >
+  <span className="justify-center">Subscribe</span>
+  </a>
+  </div>
+  </div>
+  </section>
+  </section>
   );
-}
+  }
+  
