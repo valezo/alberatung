d649fcd879ef0f0f3add8fa7c63440f33f40c3c9
Arbnore1 <bekaarbnore@gmail.com>
2024-12-15 23:34:02 +0100

    formated main

--- components/Main.js
+++ components/Main.js
@@ -1,29 +1,29 @@
 export default function Main() {
   return (
-  <section className="text-gray-600 body-font">
-  <div className="max-w-7xl mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-  <div className="lg:flex-grow md:w-1/2 md:ml-24 pt-6 flex flex-col md:items-start md:text-left mb-40 items-center text-center">
-  {/* <h1 className="sm:text-7xl text-7xl items-center Avenir xl:w-2/2 text-gray-900 mx-auto"> */}
-  {/* Kush eshte */}
-  {/* </h1> */}
-  <div className="mt-0 mb-0 mx-auto" >
-  <img src="/images/logo - homepage.jpg" width={"450px"}/>
-  </div>
-  <p className="mb-4 xl:w-2/3 text-gray-600 text-lg text-center mx-auto">
-  Kompani e cila ka për qëllim t'u ndihmojë në
-  lidhje me përgaditjen e dokumenteve që duhet dorëzuar në
-  institucione shtetërore.
-  </p>
-  <h3 className="mb-3 xl:w-2/3  text-gray-600 text-lg text-center mx-auto">
-Ne ju ofrojmë mbështetje profesionale në kohë optimale pa pasur nevojë për pritje të gjata për termine. Shumica e shërbimeve tona ofrohen online, natyrisht ne ju mirëpresim edhe në zyrën tonë.  
-</h3> 
+    <section className="text-gray-600 body-font">
+      <div className="max-w-7xl mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 md:ml-24 pt-6 flex flex-col md:items-start md:text-left mb-40 items-center text-center">
+          {/* <h1 className="sm:text-7xl text-7xl items-center Avenir xl:w-2/2 text-gray-900 mx-auto"> */}
+          {/* Kush eshte */}
+          {/* </h1> */}
+          <div className="mt-0 mb-0 mx-auto" >
+            <img src="/images/logo - homepage.jpg" width={"450px"} />
+          </div>
+          <p className="mb-4 xl:w-2/3 text-gray-600 text-lg text-center mx-auto">
+            Kompani e cila ka për qëllim t'u ndihmojë në
+            lidhje me përgaditjen e dokumenteve që duhet dorëzuar në
+            institucione shtetërore.
+          </p>
+          <h3 className="mb-3 xl:w-2/3  text-gray-600 text-lg text-center mx-auto">
+            Ne ju ofrojmë mbështetje profesionale në kohë optimale pa pasur nevojë për pritje të gjata për termine. Shumica e shërbimeve tona ofrohen online, natyrisht ne ju mirëpresim edhe në zyrën tonë.
+          </h3>
 
-<p className="mb-5 xl:w-4/5 text-black text-5xl text-center mx-auto mt-11">
-<b> Konsultohu shqip</b>
-</p> 
-<p className="mb-4 xl:w-2/3  text-gray-600 text-lg text-center mx-auto"> Me një gamë të gjerë, me përkushtim dhe profesionalizëm, ju kryeni punët tuaja shpejt, dhe më e mira, ju merreni vesh me konsultentin tuaj shqip. </p>
-{/* Me një gamë të gjerë shërbimesh, me përkushtim dhe profesionalizëm, ju kryeni punët tuaja shpejt, dhe më e mira, ju merreni vesh me konsulentin tuaj shqip. */}
-  {/* <div className="flex justify-center">
+          <p className="mb-5 xl:w-4/5 text-black text-5xl text-center mx-auto mt-11">
+            <b> Konsultohu shqip</b>
+          </p>
+          <p className="mb-4 xl:w-2/3  text-gray-600 text-lg text-center mx-auto"> Me një gamë të gjerë, me përkushtim dhe profesionalizëm, ju kryeni punët tuaja shpejt, dhe më e mira, ju merreni vesh me konsultentin tuaj shqip. </p>
+          {/* Me një gamë të gjerë shërbimesh, me përkushtim dhe profesionalizëm, ju kryeni punët tuaja shpejt, dhe më e mira, ju merreni vesh me konsulentin tuaj shqip. */}
+          {/* <div className="flex justify-center">
   <a
   className="inline-flex items-center px-5 py-3 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-gray-900"
   href="https://github.com/r1/nine4-2/"
@@ -31,16 +31,16 @@ Ne ju ofrojmë mbështetje profesionale në kohë optimale pa pasur nevojë për
   <span className="justify-center">Zbuloni më shumë →</span>
   </a>
   </div> */}
-  </div>
-  {/* <div className="xl:mr-44 sm:mr-0 sm:mb-28 mb-0 lg:mb-0 mr-48 md:pl-10">
+        </div>
+        {/* <div className="xl:mr-44 sm:mr-0 sm:mb-28 mb-0 lg:mb-0 mr-48 md:pl-10">
   <img
   className="w-80 md:ml-1 ml-24"
   alt="iPhone-12"
   src="/images/iPhone-12-Mockup.png"
   ></img>
   </div> */}
-  </div>
-  {/* <section className="mx-auto">
+      </div>
+      {/* <section className="mx-auto">
   <div className="container px-5 mx-auto lg:px-24 ">
   <div className="flex flex-col w-full mb-4 text-left lg:text-center">
   <h1 className="mb-8 text-2xl Avenir font-semibold text-black">
@@ -79,23 +79,23 @@ Ne ju ofrojmë mbështetje profesionale në kohë optimale pa pasur nevojë për
   </div>
   </div>
   </section> */}
-  <div className="grr max-w-7xl mx-auto text-center">
-  <h1 className="mb-8 text-4xl Avenir font-semibold  text-black">
-  <b> Ku gjendemi ne? </b>
-  </h1>
-  <h1 className="mb-8 text-2xl Avenir font-semibold text-gray-600 text-center">
-  Uhlbergstraße 60/1, 70794 Filderstadt, Germany
-  </h1>
-  <div className="container flex flex-col items-center justify-center mx-auto rounded-lg">
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2635.8049798155007!2d9.198313599999999!3d48.6518459!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4799e9007b73f4b5%3A0x6eb555c0025a10fd!2sAL%20Beratung!5e0!3m2!1sen!2s!4v1734215715330!5m2!1sen!2s" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-  {/* <img
+      <div className="grr max-w-7xl mx-auto text-center">
+        <h1 className="mb-8 text-4xl Avenir font-semibold  text-black">
+          <b> Ku gjendemi ne? </b>
+        </h1>
+        <h1 className="mb-8 text-2xl Avenir font-semibold text-gray-600 text-center">
+          Uhlbergstraße 60/1, 70794 Filderstadt, Germany
+        </h1>
+        <div className="container flex flex-col items-center justify-center mx-auto rounded-lg">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2635.8049798155007!2d9.198313599999999!3d48.6518459!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4799e9007b73f4b5%3A0x6eb555c0025a10fd!2sAL%20Beratung!5e0!3m2!1sen!2s!4v1734215715330!5m2!1sen!2s" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          {/* <img
   className="object-cover object-center w-3/4 mb-10 g327 border rounded-lg shadow-md"
   alt="Placeholder Image"
   src="./images/placeholder.png"
   ></img> */}
-  </div>
-  </div>
-  {/* <section className="relative">
+        </div>
+      </div>
+      {/* <section className="relative">
   <div className="max-w-6xl mx-auto px-4 sm:px-6 text-center">
   <div className="py-24 md:py-36">
   <h1 className="mb-5 text-6xl Avenir font-semibold text-gray-900">
@@ -120,7 +120,6 @@ Ne ju ofrojmë mbështetje profesionale në kohë optimale pa pasur nevojë për
   </div>
   </div>
   </section> */}
-  </section>
+    </section>
   );
-  }
-  
+}
