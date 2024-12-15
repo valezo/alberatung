7a50ef20b08b9314295c746aa102b22255c32dc5
arbrim <arbrimrashiti@gmail.com>
2024-12-15 22:39:26 +0100

    OpenSans at global styles and watermark to 2024

--- components/Footer.js
+++ components/Footer.js
@@ -192,7 +192,7 @@ export default function Footer() {
               href="/"
               className="text-md text-black transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold tracking-tight"
             >
-              © 2024/2025
+              © 2024
             </a>
           </ul>
         </div>
